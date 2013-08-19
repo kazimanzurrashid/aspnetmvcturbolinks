d46f9323cc7f7a46de46c2dd2dab66cd582e9517
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2013-08-19 20:27:02 +0600

    Updated nuget package and sample app.

---
+++ Source/TurboLinksTest/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿/* jshint browser: true */
+/* global jQuery: false, Turbolinks: false  */
+
+(function ($, Turbolinks) {
+    'use strict';
+    
+    window.visitRoot = function() {
+        Turbolinks.visit('/');
+    };
+
+    window.visitProject = function(id) {
+        var url = '/projects/details/' + id;
+        Turbolinks.visit(url);
+    };
+
+    $(document).on('page:load', function () {
+        $('form').each(function () {
+            $.data(this, 'validator', void (0));
+        });
+        $.validator.unobtrusive.parse('form');
+    });
+})(jQuery, Turbolinks);
